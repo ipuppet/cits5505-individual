22fac064aca8dc069757f1e02f315507dc0a2aed
Samuel <zimingw.ol@outlook.com>
2025-04-04 05:55:46 +0800

    Update CV page with new layout, enhanced styling, and JavaScript functionality for navbar behavior

---
+++ js/cv.js
@@ -0,0 +1,11 @@
+const navbar = document.querySelector(".navbar")
+const navHomeBtn = document.querySelector(".nav-links li:first-child")
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled")
+        navHomeBtn.style.display = "block"
+    } else {
+        navbar.classList.remove("scrolled")
+        navHomeBtn.style.display = "none"
+    }
+})
